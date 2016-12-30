82807f31ff48ef5debb19ff5ef293201fdbd6dd2
wyc <wuyanchang@hotmail.com>
2016-12-30 01:46:56 +0000

    add xx-net&aria2

--- tools/gohosts.sh
+++ tools/gohosts.sh
@@ -3,7 +3,20 @@
 GOHOSTS_URL="https://raw.githubusercontent.com/racaljk/hosts/master/hosts"
 
 # Backup hosts
-cp /etc/hosts $HOME/GoHosts/hosts-$(date '+%Y-%m-%d').back
+BACKUP_HOME=$HOME/GoHosts
+BACKUP_NAME=hosts-$(date '+%Y-%m-%d')
+if [ ! -e $BACKUP_HOME ]; then
+    mkdir $BACKUP_HOME
+fi
+if [ -e $BACKUP_HOME/$BACKUP_NAME ]; then
+    i=0
+    while [ -e $BACKUP_HOME/$BACKUP_NAME.$i ]; do
+        i=`expr $i + 1`
+    done
+    BACKUP_NAME=$BACKUP_NAME.$i
+fi
+cp /etc/hosts $BACKUP_HOME/$BACKUP_NAME
+echo "Backup in $BACKUP_HOME/$BACKUP_NAME"
 
 # Delete outdated google hosts
 sudo sed -i "/^# Modified hosts start/,/^# Modified hosts end/d" /etc/hosts
---
+++ xx-net.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+XXNET_TARBALL=https://api.github.com/repos/XX-net/XX-Net/tarball/master
+XXNET_HOME=$HOME/.XX-Net
+
+if [ ! -d $XXNET_HOME ]; then
+    mkdir $XXNET_HOME
+fi
+
+curl -L $XXNET_TARBALL | tar xz -C $XXNET_HOME --strip-components=1
+
+if [ -e $XXNET_HOME/start ]; then
+    sed -i.bak "s#\$SCRIPTPATH/start > /dev/null#xterm -e '\$SCRIPTPATH/start'#g" $XXNET_HOME/start
+    read -p "Is start XX-Net now(Y/n)?" yn
+    yn=${yn:-y}
+    case $yn in
+        [Nn]* ) echo "Enter the following command to execute:\n \"$XXNET_HOME/start\"";;
+        [Yy]* ) $XXNET_HOME/start;;
+    esac
+fi
