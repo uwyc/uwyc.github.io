6869e78910e43f71dd295b249873227762275a2d
Wu Yan Chang <wuyanchang@hotmail.com>
2017-05-29 22:13:02 +0800

    update xx-net.sh

--- internet/xx-net.sh
+++ internet/xx-net.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 XXNET_TARBALL=https://api.github.com/repos/XX-net/XX-Net/tarball/master
+XXNET_STABLE_VERSION=3.3.1
+XXNET_DOWNLOAD_URL=https://codeload.github.com/XX-net/XX-Net/zip/$XXNET_STABLE_VERSION
+XXNET_DIR=XX-Net-$XXNET_STABLE_VERSION
 XXNET_HOME=$HOME/.local/share/XX-Net
 
-if [ ! -d $XXNET_HOME ]; then
-    mkdir $XXNET_HOME
-fi
+# if [ ! -d $XXNET_HOME ]; then
+#     mkdir -p $XXNET_HOME
+# fi
 
 echo "XX-Net Downloading..."
-wget -O- $XXNET_TARBALL | tar xz -C $XXNET_HOME --strip-components=1
+# wget -O- $XXNET_TARBALL | tar xz -C $XXNET_HOME --strip-components=1
+wget $XXNET_DOWNLOAD_URL -qO /tmp/$XXNET_DIR.zip
+
+echo "Extracting $XXNET_DIR.zip...."
+unzip -q /tmp/$XXNET_DIR.zip -d /tmp
+mv /tmp/$XXNET_DIR $XXNET_HOME
 
 if [ -e $XXNET_HOME/start ]; then
-    sed -i.bak "s#\$SCRIPTPATH/start > /dev/null#xterm -e '\$SCRIPTPATH/start'#g" $XXNET_HOME/start
-    read -p "Is start XX-Net now(Y/n)?" yn
-    yn=${yn:-y}
-    case $yn in
-        [Nn]* ) echo "Enter the following command to execute:\n \"$XXNET_HOME/start\"";;
-        [Yy]* ) $XXNET_HOME/start;;
-    esac
+    sed -i.bak "s#\$SCRIPTPATH/start > /dev/null#x-terminal-emulator -e '\$SCRIPTPATH/start'#g;s#DESKDIR=~/Desktop/#DESKDIR=~/.local/share/applications/#g" $XXNET_HOME/start
+    mkdir -p $HOME/.local/share/applications
+    $XXNET_HOME/start;
 fi
